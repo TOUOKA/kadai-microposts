81e217b32f6559d4ffc7df016ea976f78960143b
kazuya.touoka <tookakazuya@gmail.com>
2019-05-28 10:20:42 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,17 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
-    
+  end
+  
+  def followings
+    @user =User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
 
   private
@@ -32,5 +42,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end    
-
 end
