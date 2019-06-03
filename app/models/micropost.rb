d08df3c689edc0a8df165489df5e58bab058b7be
kazuya.touoka <tookakazuya@gmail.com>
2019-06-03 08:07:22 +0000

    second commit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,15 +5,5 @@ class Micropost < ApplicationRecord
   has_many :favorites ,foreign_key: 'micropost_id', dependent: :destroy
   has_many :users, through: :favorites
   
-  # マイクロポストをいいねする
-  #def likes(user)
-   # likes.create(user_id: user.id)
-  #end
-  
-  # マイクロポストのいいねを解除する
-  #def unlikes(user)
-  #  likes.find_by(user_id: user.id).destory
-  #end
-  
   validates :content, presence: true, length: { maximum: 255}
 end
